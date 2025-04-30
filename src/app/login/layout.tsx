ffe6bb919d62fda4f0b91c2c8a41e555d04acc65
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-04-30 13:54:00 +0900

    feat: login layout

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Instagram Clone Login",
+  description: "Instagram Clone Login",
+};
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <main>{children}</main>;
+}
