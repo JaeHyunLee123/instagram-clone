b54d4119dd18bf9691aa119f73fdff995029fa5f
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-04-30 17:52:59 +0900

    feat: register frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
+import Providers from "@/provider/Providers";
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ["latin", "latin-ext"],
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${notoSansKR.className} antialiased`}>
-        <main>{children}</main>
+        <Providers>
+          <main>{children}</main>
+        </Providers>
       </body>
     </html>
   );
--- src/compontents/RegisterForm.tsx
+++ src/compontents/RegisterForm.tsx
@@ -6,28 +6,83 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-regular-svg-icons/faEye";
 import { faEyeSlash } from "@fortawesome/free-regular-svg-icons/faEyeSlash";
 import AuthInput from "./ui/AuthInput";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { registerUser } from "@/actions/auth";
 
-interface FormData {
-  email: string;
-  password: string;
-  passwordConfirmation: string;
-  nickName: string;
-}
+const registerSchema = z
+  .object({
+    email: z.string().email({ message: "부정확한 이메일 형식입니다." }),
+    nickname: z.string().min(2, { message: "최소 2글자 이상 입력해주세요." }),
+    password: z.string().min(6, { message: "최소 6글자 이상 입력해주세요." }),
+    passwordConfirmation: z.string(),
+  })
+  .superRefine(({ password, passwordConfirmation }, ctx) => {
+    if (password !== passwordConfirmation) {
+      ctx.addIssue({
+        code: "custom",
+        path: ["passwordConfirmation"],
+        message: "비밀번호가 서로 다릅니다.",
+      });
+    }
+  });
 
 const RegisterForm = () => {
-  const {} = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    mode: "onChange",
+  });
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const [isPasswordConfirmationVisible, setIsPasswordConfirmationVisible] =
     useState(false);
 
+  const { mutate, isPending } = useMutation({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
+      if (data.code === "OK") {
+      } else if (data.code === "EMAIL_DUPLICATE") {
+      } else {
+      }
+    },
+    onError: () => {},
+  });
+
+  const onSubmit = ({
+    email,
+    nickname,
+    password,
+  }: z.infer<typeof registerSchema>) => {
+    mutate({ email, password, nickname });
+  };
+
   return (
-    <form className="flex flex-col space-y-2 w-full max-w-sm">
-      <AuthInput placeholder="Email" type="email" />
-      <AuthInput placeholder="Nickname" type="text" />
+    <form
+      className="flex flex-col space-y-2 w-full max-w-sm"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <AuthInput
+        placeholder="Email"
+        type="email"
+        {...register("email")}
+        errorMessage={errors.email?.message}
+      />
+      <AuthInput
+        placeholder="Nickname"
+        type="text"
+        {...register("nickname")}
+        errorMessage={errors.nickname?.message}
+      />
       <div className="flex items-center space-x-2">
         <AuthInput
           placeholder="Password"
           type={isPasswordVisible ? "text" : "password"}
+          {...register("password")}
+          errorMessage={errors.password?.message}
         />
         <button
           type="button"
@@ -47,6 +102,8 @@ const RegisterForm = () => {
         <AuthInput
           placeholder="Password Confirmation"
           type={isPasswordConfirmationVisible ? "text" : "password"}
+          {...register("passwordConfirmation")}
+          errorMessage={errors.passwordConfirmation?.message}
         />
         <button
           type="button"
@@ -62,8 +119,11 @@ const RegisterForm = () => {
           )}
         </button>
       </div>
-      <button className="bg-blue-400 text-white font-semibold p-2 text-center rounded mt-4 transition-transform active:scale-95  hover:cursor-pointer">
-        회원가입
+      <button
+        disabled={isPending}
+        className={`bg-blue-400 text-white font-semibold p-2 text-center rounded mt-4 transition-transform active:scale-95  hover:cursor-pointer`}
+      >
+        {isPending ? "서버와 통신 중 입니다." : "회원가입"}
       </button>
     </form>
   );
