e4396c888d1f40afb8650c366f233b0a07d6255f
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-02 17:36:19 +0900

    feat: login frontend

--- src/compontents/LoginForm.tsx
+++ src/compontents/LoginForm.tsx
@@ -6,23 +6,71 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-regular-svg-icons/faEye";
 import { faEyeSlash } from "@fortawesome/free-regular-svg-icons/faEyeSlash";
 import AuthInput from "./ui/AuthInput";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { emailLogin } from "@/actions/auth";
+import { useRouter } from "next/navigation";
 
-interface FormData {
-  email: string;
-  password: string;
-}
+const loginSchema = z.object({
+  email: z.string().email({ message: "부정확한 이메일 형식입니다." }),
+  password: z.string().min(6, { message: "최소 6글자 이상 입력해주세요." }),
+});
 
 const LoginForm = () => {
-  const {} = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: emailLogin,
+    onSuccess: (data) => {
+      if (data.code === "OK") {
+        console.log("ok");
+        router.push("/");
+      } else if (data.code === "INCORRECT_PASSWORD") {
+        console.log("p");
+      } else if (data.code === "NO_USER") {
+        console.log("u");
+      } else if (data.code === "SOCIAL_USER") {
+      } else if (data.code === "UNKOWN_ERROR") {
+        console.log("e");
+      } else {
+        console.log("e");
+      }
+    },
+    onError: () => {},
+  });
+
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
+  const onSubmit = ({ email, password }: z.infer<typeof loginSchema>) => {
+    mutate({ email, password });
+  };
+
   return (
-    <form className="flex flex-col space-y-2 w-full max-w-sm">
-      <AuthInput placeholder="Email" type="email" />
+    <form
+      className="flex flex-col space-y-2 w-full max-w-sm"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <AuthInput
+        placeholder="Email"
+        type="email"
+        {...register("email")}
+        errorMessage={errors.email?.message}
+      />
       <div className="flex items-center space-x-2">
         <AuthInput
           placeholder="Password"
           type={isPasswordVisible ? "text" : "password"}
+          {...register("password")}
+          errorMessage={errors.password?.message}
         />
         <button
           type="button"
@@ -38,8 +86,11 @@ const LoginForm = () => {
           )}
         </button>
       </div>
-      <button className="bg-blue-400 text-white font-semibold p-2 text-center rounded mt-4 transition-transform active:scale-95  hover:cursor-pointer">
-        로그인
+      <button
+        disabled={isPending}
+        className="bg-blue-400 text-white font-semibold p-2 text-center rounded mt-4 transition-transform active:scale-95  hover:cursor-pointer"
+      >
+        {isPending ? "서버와 통신 중입니다." : "로그인"}
       </button>
     </form>
   );
