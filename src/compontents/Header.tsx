e33e7361d26231f50c4d65ad5bc11b310da06f60
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-08 10:58:37 +0900

    fix: change Header to client component

--- src/compontents/Header.tsx
+++ src/compontents/Header.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
-import { getSession } from "@/lib/session";
+
 import Link from "next/link";
 import Button from "./ui/Button";
+import { useQuery } from "@tanstack/react-query";
+import { getSessionClient } from "@/actions/auth";
 
-export default async function Header() {
-  const session = await getSession();
+export default function Header() {
+  const { data, isPending } = useQuery({
+    queryKey: ["sesssion"],
+    queryFn: getSessionClient,
+  });
 
   return (
     <header className="bg-white w-full flex items-center justify-center px-5 py-6">
@@ -21,9 +28,11 @@ export default async function Header() {
       </Link>
 
       <div className="w-[33%] flex items-center justify-end space-x-4">
-        {session ? (
+        {isPending ? (
+          <></>
+        ) : data && data.session ? (
           <>
-            <span>{`Hi, ${session.nickname}`}</span>
+            <span>{`Hi, ${data.session.nickname}`}</span>
             <Button>Logout</Button>
           </>
         ) : (
