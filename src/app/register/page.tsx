f6bd17566d692bc0e1155bde04bdcfa3652290ec
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-05-13 15:29:18 +0900

    feat: social login frontend (#12)

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,5 @@
 import RegisterForm from "@/compontents/RegisterForm";
-import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SocialMediaLoginButton from "@/compontents/ui/SocialMediaLoginButton";
 import Link from "next/link";
 
 export default function Register() {
@@ -9,10 +8,7 @@ export default function Register() {
       <h1 className="text-white text-4xl font-bold">Instagram Clone</h1>
       <RegisterForm />
       <span className="text-white">or</span>
-      <div className="flex items-center text-white space-x-1 hover:cursor-pointer">
-        <FontAwesomeIcon icon={faGithub} size="xl" />
-        <span className="font-medium">깃허브로 회원가입하기</span>
-      </div>
+      <SocialMediaLoginButton />
       <div className="w-4/5 max-w-sm  border-b border-white" />
       <div className="flex flex-col items-center space-y-2 text-white">
         <span className="text-sm">이미 계정이 있으신가요?</span>
